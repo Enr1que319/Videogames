22a6d65504d4ecc6f43e63335e3d6aeb25a50b76
Enr1que319 <59896508+Enr1que319@users.noreply.github.com>
2020-07-04 16:32:38 -0500

    Upload new files

---
+++ scripts/config.py
@@ -0,0 +1,1 @@
+API_KEY = "3e7a08acbemsh701736c3242af0dp162838jsn2ba0c24931c5"
---
+++ static/js/index.js
@@ -0,0 +1,101 @@
+var genres = {
+  url: "http://127.0.0.1:5000/api/v1/genres",
+  type: "GET",
+  dataType: "json",
+};
+
+var games = {
+  url: "http://127.0.0.1:5000/api/v1/games",
+  type: "GET",
+  dataType: "json",
+};
+
+var top3VgmYear = {
+  url: "http://127.0.0.1:5000/api/v1/top3vgs_peryear",
+  type: "GET",
+  dataType: "json",
+};
+
+var top3VgenSalesComp = {
+  url: "http://127.0.0.1:5000/api/v1/top3gensales_percomp",
+  type: "GET",
+  dataType: "json",
+};
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function init() {
+  $.ajax(top3VgmYear).done((top3vdgYr) => {
+    $.ajax(top3VgenSalesComp).done((top3genSaComp) => {
+      var year = [];
+      top3vdgYr[0]["data"][0].forEach((element) => {
+        year.push(element.year);
+      });
+
+      var uniqueYear = year.filter(function (item, pos) {
+        return year.indexOf(item) == pos;
+      });
+
+      uniqueYear.forEach((object) =>
+        d3.select("#selDataset").append("option").text(object)
+      );
+
+      var dropdown_value = d3.select("#selDataset").property("value");
+
+      top3vdgYr[0]["data"][0].forEach((element) => {
+        if (dropdown_value == element.year) {
+          d3.select("#sample-metadata")
+            .append("p")
+            .text(`${element.name} (${element.company})`);
+        }
+      });
+      //Aqui va la lineal
+    });
+  });
+}
+
+$.ajax(genres).done((genresData) => {
+  $.ajax(games).done((gamesData) => {
+    var all = [];
+    genresData[0]["data"].forEach((gen) => {
+      var genre = gen.name;
+      var ratings = [];
+      var metacritics = [];
+      for (let index = 0; index < 3; index++) {
+        var gameId = gen.top_games[index].id;
+        gamesData[0]["data"].forEach((gm) => {
+          if (gameId == gm.id) {
+            ratings.push(gm.rating);
+            metacritics.push(gm.metacritic);
+          }
+        });
+      }
+      // all.push({ [genre]: gamesId });
+      all.push({
+        Genre: genre,
+        Ratings: ratings,
+        Metacritics: metacritics,
+      });
+    });
+  });
+});
+
+function change() {
+  $.ajax(top3VgmYear).done((top3vdgYr) => {
+    var dropdown_value = d3.select("#selDataset").property("value");
+    d3.select("#sample-metadata").text("");
+
+    top3vdgYr[0]["data"][0].forEach((element) => {
+      if (dropdown_value == element.year) {
+        d3.select("#sample-metadata")
+          .append("p")
+          .text(`${element.name} (${element.company})`);
+      }
+    });
+  });
+}
+
+d3.selectAll("#selDataset").on("change", change);
+init();
